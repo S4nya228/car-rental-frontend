104db4c2402652fd3a7ad26d73c837911edff834
S4nya228 <gyzica40@gmail.com>
2024-05-13 19:37:55 +0300

    fix breadcrumb

--- src/components/Breadcrumb/index.jsx
+++ src/components/Breadcrumb/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function Breadcrumb() {
+	const pathNamesMap = {
+		'/': 'Головна',
+		'/car-fleet': 'Автопарк',
+		'/about-us': 'Про нас',
+		'/profile': 'Акаунт',
+		'/profile/my-order': 'Мої бронювання',
+	}
+	const location = useLocation()
+	const pathnames = location.pathname.split('/').filter((x) => x)
+
 	return (
 		<div className="breadcrumb">
 			<div className="container">
@@ -10,8 +20,25 @@ function Breadcrumb() {
 					<Link className="breadcrumb__link-to-main" to="/">
 						Головна
 					</Link>
-					<span>/</span>
-					<span className="breadcrumb__active-page">Автомобілі</span>
+					{pathnames.map((name, index) => {
+						const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`
+						const isLast = index === pathnames.length - 1
+						const displayName = pathNamesMap[routeTo] || name
+						return (
+							<React.Fragment key={index}>
+								<span>/</span>
+								{isLast ? (
+									<span className="breadcrumb__active-page">{displayName}</span>
+								) : (
+									<React.Fragment>
+										<Link to={routeTo} className="breadcrumb__link">
+											{displayName}
+										</Link>
+									</React.Fragment>
+								)}
+							</React.Fragment>
+						)
+					})}
 				</div>
 			</div>
 		</div>
