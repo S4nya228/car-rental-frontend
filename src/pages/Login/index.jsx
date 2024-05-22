e28176579918dac93850d666b9aef8da1a28c5cb
S4nya228 <gyzica40@gmail.com>
2024-05-22 14:44:51 +0300

    add login functional

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.scss'
 import { Link, useNavigate } from 'react-router-dom'
+import axiosInstance from '../../api/axiosInstance'
+import { useDispatch } from 'react-redux'
+import { setTokens } from '../../store/authSlice'
 
 function Login() {
 	const navigate = useNavigate()
+	const dispatch = useDispatch()
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const handleLogin = async (e) => {
+		e.preventDefault()
+
+		try {
+			const response = await axiosInstance.post('/login', {
+				email: email,
+				password: password,
+			})
+
+			const { token, refreshToken } = response.data
+			dispatch(setTokens({ token: token, refreshToken: refreshToken }))
+			navigate('/')
+		} catch (error) {
+			console.log('Error: ', error)
+		}
+	}
 
 	const handleBack = () => {
 		navigate('/')
@@ -22,40 +45,49 @@ function Login() {
 						<a href="#">Політику конфіденційності</a>.
 					</span>
 				</div>
-				<div className="login__inputs">
-					<div className="login__email">
-						<div className="login__input-container">
-							<input type="text" name="email" id="email-label" required />
-							<label htmlFor="email-label">Пошта</label>
+				<form onSubmit={handleLogin}>
+					<div className="login__inputs">
+						<div className="login__email">
+							<div className="login__input-container">
+								<input
+									type="text"
+									name="email"
+									id="email-label"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									required
+								/>
+								<label htmlFor="email-label">Пошта</label>
+							</div>
+						</div>
+						<div className="login__password">
+							<div className="login__input-container">
+								<input
+									type="password"
+									name="password"
+									value={password}
+									id="password-label"
+									onChange={(e) => setPassword(e.target.value)}
+									required
+								/>
+								<label htmlFor="password-label">Пароль</label>
+							</div>
 						</div>
 					</div>
-					<div className="login__password">
-						<div className="login__input-container">
-							<input
-								type="password"
-								name="password"
-								id="password-label"
-								required
-							/>
-							<label htmlFor="password-label">Пароль</label>
+					<div className="login__etc">
+						<div className="login__member">
+							<span>
+								Немає акаунту? <Link to="/register">Зареєструватися</Link>
+							</span>
+						</div>
+						<div className="login__remember-password">
+							<span>
+								Забули <a href="#">пароль</a>?
+							</span>
 						</div>
 					</div>
-				</div>
-				<div className="login__etc">
-					<div className="login__member">
-						<span>
-							Немає акаунту? <Link to="/register">Зареєструватися</Link>
-						</span>
-					</div>
-					<div className="login__remember-password">
-						<span>
-							Забули <a href="#">пароль</a>?
-						</span>
-					</div>
-				</div>
-				<button className="login__button-login" type="button">
-					Увійти
-				</button>
+					<button className="login__button-login">Увійти</button>
+				</form>
 			</div>
 		</div>
 	)
